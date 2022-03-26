54d827552894d233e13500407aa01cff0eb4a259
specbug <rishit7.rv@gmail.com>
2022-03-26 13:44:33 +0530

    sixeleven

--- worker.js
+++ worker.js
@@ -227,9 +227,9 @@
       div.notion-topbar > div > div:nth-child(5) { display: none !important; }
       div.notion-topbar > div > div:nth-child(6) { display: none !important; }
       div.notion-topbar > div > div:nth-child(7) { display: none !important; }
-    //   div.notion-topbar-mobile > div:nth-child(3) { display: none !important; }
-    //   div.notion-topbar-mobile > div:nth-child(4) { display: block !important; }
-    //   div.notion-topbar-mobile > div:nth-child(5) { display: none !important; }
+      div.notion-topbar-mobile > div:nth-child(3) { display: none !important; }
+      div.notion-topbar-mobile > div:nth-child(4) { display: none !important; }
+      div.notion-topbar-mobile > div:nth-child(5) { display: none !important; }
       div.notion-topbar > div > div:nth-child(1n).toggle-mode { display: block !important; }
       div.notion-topbar-mobile > div:nth-child(1n).toggle-mode { display: block !important; }
       </style>`, {
